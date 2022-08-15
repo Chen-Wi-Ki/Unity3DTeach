15f0ae0c674f9da2ddd513bab1017eb89dbc9426
陳威齊 <chenwici@stust.edu.tw>
2022-08-15 14:14:36 +0800

    註釋調碰撞事件中的Destroy行(關掉碰撞後消失功能),改成碰撞後Square2縮小並隨機順移到畫面上某處,另外也在此調整全部物件的整體配色。

--- TeachProject/Assets/C#/Square2TriggerEvent.cs
+++ TeachProject/Assets/C#/Square2TriggerEvent.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class Square2TriggerEvent : MonoBehaviour
 {
+    public GameObject NewSquare2;
+    Vector3 NewObjLocation;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        float X= Random.value * 8.4f,
+              Y= Random.value * 4f,
+              Z =0;
+
+        if (Random.value < 0.5)
+        {
+            X = X * -1;
+        }
+
+        if (Random.value < 0.5)
+        {
+            Y = Y * -1;
+        }
+
+        NewObjLocation = new Vector3(X,Y,Z);
+
         if (other.gameObject.name == "Square1")
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            transform.localScale = new Vector3(transform.localScale.x / 1.5f, transform.localScale.y / 1.5f, transform.localScale.z);
+            transform.position = NewObjLocation;
         }
     }
 }
