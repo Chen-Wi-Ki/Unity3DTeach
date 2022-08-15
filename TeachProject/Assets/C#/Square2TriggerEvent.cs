ec61dd3f20a61f12a5381b09b5b3ffd585dd8c34
陳威齊 <chenwici@stust.edu.tw>
2022-08-15 15:18:07 +0800

    防誤判機制1.未控制Floor1時就吃到下一顆Square2之狀況,方法是根據亂數產生後下一點Y值判斷它是否在Square1的2倍大小的Y值範圍內,若有就再重新生成亂數Y值再判斷,直到沒有為止才生生成下一個瞬移點

--- TeachProject/Assets/C#/Square2TriggerEvent.cs
+++ TeachProject/Assets/C#/Square2TriggerEvent.cs
@@ -7,7 +7,6 @@ public class Square2TriggerEvent : MonoBehaviour
     public GameObject NewSquare2;
     Vector3 NewObjLocation;
     public int Level;
-
     private void Start()
     {
         Level = 13;
@@ -15,24 +14,33 @@ public class Square2TriggerEvent : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        float X= Random.value * 8.4f,
-              Y= Random.value * 4f,
-              Z =0;
-
-        if (Random.value < 0.5)
-        {
-            X = X * -1;
-        }
-
-        if (Random.value < 0.5)
-        {
-            Y = Y * -1;
-        }
-
-        NewObjLocation = new Vector3(X,Y,Z);
-
+        
         if (other.gameObject.name == "Square1")
         {
+            
+            float X = Random.value * 8.4f,
+                Y = Random.value * 4f,
+                Z = 0;
+
+            if (Random.value < 0.5)
+            {
+                X = X * -1;
+            }
+
+            if (Random.value < 0.5)
+            {
+                Y = Y * -1;
+            }
+
+            while ((Y <= other.gameObject.transform.position.y + 1.5f) && (Y >= other.gameObject.transform.position.y - 1.5f))
+            {
+                Y = Random.value * 4f;
+                if (Random.value < 0.5)
+                {
+                    Y = Y * -1;
+                }
+            }
+            NewObjLocation = new Vector3(X,Y,Z);
             //Destroy(gameObject);
             transform.localScale = new Vector3(transform.localScale.x / 1.5f, transform.localScale.y / 1.5f, transform.localScale.z);
             transform.position = NewObjLocation;
