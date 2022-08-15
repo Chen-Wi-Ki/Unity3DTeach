7969af71d21f3a217db6d3b9785149948f69ed02
陳威齊 <chenwici@stust.edu.tw>
2022-08-15 10:39:15 +0800

    Square1刪除動畫控件(animation及animator)後,新增C#腳本並自行設計左右移動方法後,觀測Play效果

---
+++ TeachProject/Assets/C#/Square1MoveEvent.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square1MoveEvent : MonoBehaviour
+{
+    int MoveFlag1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveFlag1 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoveFlag1 == 0)
+        {
+            if (transform.position.x <= 8.4)
+            {
+                transform.position = new Vector3(transform.position.x + 0.05f, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                MoveFlag1 = 1;
+            }
+        }
+
+        if (MoveFlag1 == 1)
+        {
+            if (transform.position.x >= -8.4)
+            {
+                transform.position = new Vector3(transform.position.x - 0.05f, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                MoveFlag1 = 0;
+            }
+        }
+    }
+}
