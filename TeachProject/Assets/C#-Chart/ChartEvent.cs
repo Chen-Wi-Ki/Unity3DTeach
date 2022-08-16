18eb80bcd4d715d1455c01f222cfe898eabd5954
陳威齊 <chenwici@stust.edu.tw>
2022-08-16 18:05:00 +0800

    建立一個新的Scene,並在裡面新增Chart控件,設計3條時序線(分別為UpEvent/DownEvent/AnalogEvent),新增C#腳本ChartEvent,設計使用Chart控制3條時序線一時序監控使用者輸入訊號

---
+++ TeachProject/Assets/C#-Chart/ChartEvent.cs
@@ -0,0 +1,75 @@
+using ChartAndGraph;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+
+public class ChartEvent : MonoBehaviour
+{
+    public GraphChart Mychart;
+    int Time_x;
+
+    SerialPort _SerialPort;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mychart = gameObject.GetComponent<GraphChart>();
+        Mychart.DataSource.ClearCategory("UpEvent");
+        Mychart.DataSource.ClearCategory("DownEvent");
+
+        _SerialPort = new SerialPort("COM7", 115200);
+        _SerialPort.Open();
+        _SerialPort.ReadTimeout = 10;
+        _SerialPort.RtsEnable = true;
+        _SerialPort.DtrEnable = true;
+
+    }
+    float TempData;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey("up"))
+        {
+            Mychart.DataSource.AddPointToCategoryRealtime("UpEvent", Time_x,1);
+        }
+        else
+        {
+            Mychart.DataSource.AddPointToCategoryRealtime("UpEvent", Time_x, 0);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            Mychart.DataSource.AddPointToCategoryRealtime("DownEvent", Time_x, 1);
+        }
+        else
+        {
+            Mychart.DataSource.AddPointToCategoryRealtime("DownEvent", Time_x, 0);
+        }
+        
+        
+        if (_SerialPort.IsOpen)
+        {
+            try
+            {
+                TempData = float.Parse(_SerialPort.ReadLine()) / 1000;
+
+                Mychart.DataSource.AddPointToCategoryRealtime("AnalogEvent", Time_x, TempData);
+
+                GC.Collect();
+            }
+            catch
+            {
+                Debug.Log("overflow!");
+                GC.Collect();
+            }
+        }
+        Time_x++;
+    }
+
+    private void OnDisable()
+    {
+        _SerialPort.Close();
+    }
+}
