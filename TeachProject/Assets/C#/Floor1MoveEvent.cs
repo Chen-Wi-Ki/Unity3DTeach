7c7f9879642184d711dead649aadf8ac6cfe15c5
陳威齊 <chenwici@stust.edu.tw>
2022-08-16 16:13:39 +0800

    調整Floor1位置與大小,並修改移動方法為analog形式(也處理了映射與濾波),另外將此輸出windows版;但目前僅單一COMPort能用,需再設計自掃描功能。

--- TeachProject/Assets/C#/Floor1MoveEvent.cs
+++ TeachProject/Assets/C#/Floor1MoveEvent.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
+using System;
 
 public class Floor1MoveEvent : MonoBehaviour
 {
     // Start is called before the first frame update
+    SerialPort _SerialPort;
     void Start()
     {
-        
+        _SerialPort = new SerialPort("COM7",115200);
+        _SerialPort.Open();
+        _SerialPort.ReadTimeout = 10;
+        _SerialPort.RtsEnable = true;
+        _SerialPort.DtrEnable = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float TempData;
+        /*
         if (Input.GetKey("up"))
         {
             if (transform.position.y <= 3.8)
@@ -28,5 +37,31 @@ public class Floor1MoveEvent : MonoBehaviour
                 transform.position = new Vector3(transform.position.x , transform.position.y - Time.deltaTime * 25, transform.position.z);
             }
         }
+        */
+        if (_SerialPort.IsOpen)
+        {
+            try 
+            {
+                
+                TempData=float.Parse( _SerialPort.ReadLine())/1000;
+                TempData = ((TempData / 5) * (-6f + 17.5f)) - 17.5f;//mapping 0~5  ->  -6~-17.5
+                //print(TempData);
+                if (TempData < -15.5f)//High Filter
+                {
+                    TempData = -15.5f;
+                }
+                transform.position = new Vector3(transform.position.x, TempData, transform.position.z);
+            }
+            catch (TimeoutException)
+            {
+                //Debug.Log("overflow!");
+            }
+        }
+        GC.Collect();
+    }
+
+    private void OnDisable()
+    {
+        _SerialPort.Close();
     }
 }
